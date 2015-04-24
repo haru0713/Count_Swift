3594359b0c1dad1c22507e774787bd3dc178a61c
阿部祐樹 <ABE@abeyuuki-no-MacBook-Air.local>
2015-04-24 20:27:41 +0900

    いえい

--- countswift1/ViewController.swift
+++ countswift1/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +22,77 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    @IBAction func plus(){
+        number=number+1
+        label.text=String(number)
+    
+        if number <= -10{
+            label.textColor = UIColor.blueColor()
+        }else if number >= 10{
+            label.textColor = UIColor.redColor()
+        }else{
+            
+            label.textColor = UIColor.blackColor()
+        }
+        
+
+    }
+    
+    @IBAction func m(){
+        number=number-1
+        label.text=String(number)
+        if number <= -10{
+            label.textColor = UIColor.blueColor()
+        }else if number >= 10{
+            label.textColor = UIColor.redColor()
+        }else{
+            
+            label.textColor = UIColor.blackColor()
+        }
+        
+    
+    }
+    
+    @IBAction func k(){
+        number=number*2
+        label.text=String(number)
+        if number <= -10{
+            label.textColor = UIColor.blueColor()
+        }else if number >= 10{
+            label.textColor = UIColor.redColor()
+        }else{
+            
+            label.textColor = UIColor.blackColor()
+        }
+        
+    
+    }
+    
+    @IBAction func w(){
+        number=number/2
+        label.text=String(number)
+        if number <= -10{
+            label.textColor = UIColor.blueColor()
+        }else if number >= 10{
+            label.textColor = UIColor.redColor()
+        }else{
+        
+        label.textColor = UIColor.blackColor()
+        }
+
+    
+    }
+
+    @IBAction func c(){
+        number=number-number
+        label.text=String(number)
+    label.textColor = UIColor.blackColor()
+    }
+
+    
+
+
 
 
 }
